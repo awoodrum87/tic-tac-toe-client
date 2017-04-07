eb1d549624cd7d2cac519c82b979bb08d9a4224c
awoodrum87 <awoodrum87@gmail.com>
2017-04-07 18:05:08 -0400

    game winning logic complete. draw logic not yet complete

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -20,12 +20,13 @@ $(() => {
 })
 
 const squaresPlayed = []
+
 // log the square that was clicked
 $('.gameBoard').on('click', '.boardPiece', function (event) {
   // registers the current click
   const currentClick = $(event.currentTarget)
   // gets the index of the square that was just clicked
-  const indexOfSquare = $('.boardPiece').index(currentClick)
+  // const indexOfSquare = $('.boardPiece').index(currentClick)
   // gets the X or O
   const stringInHTML = $(currentClick).html()
   if (stringInHTML === 'X') {
@@ -33,39 +34,23 @@ $('.gameBoard').on('click', '.boardPiece', function (event) {
   } else {
     squaresPlayed.push('O')
   }
-  console.log(squaresPlayed)
   console.log('X or O in the DOM?:', stringInHTML)
-  console.log('indexOfSquare is: ', indexOfSquare)
-  console.log(winner())
+  // console.log('indexOfSquare is: ', indexOfSquare)
 })
-const winner = function () {
-  const boxZero = $('#box-0').html()
-  const boxOne = $('#box-1').html()
-  const boxTwo = $('#box-2').html()
-  const boxThree = $('#box-3').html()
-  const boxFour = $('#box-4').html()
-  const boxFive = $('#box-5').html()
-  const boxSix = $('#box-6').html()
-  const boxSeven = $('#box-7').html()
-  const boxEight = $('#box-8').html()
+const determineWinner = function (gameBoard, playerSymbol) {
 // logic to determine winners
-  if (boxZero !== '' && (boxZero === boxOne) && (boxZero === boxTwo)) {
-    return 'row 1 is winners'
-  } else if (boxThree !== '' && (boxThree === boxFour) && (boxThree === boxFive)) {
-    return 'row 2 is winner'
-  } else if (boxSix !== '' && (boxSix === boxSeven) && (boxSix === boxEight)) {
-    return 'row 3 is winner'
-  } else if (boxZero !== '' && (boxZero === boxThree) && (boxZero === boxSix)) {
-    return 'collumn 1 is winner'
-  } else if (boxOne !== '' && (boxOne === boxFour) && (boxOne === boxSeven)) {
-    return ('collumn 2 is winner')
-  } else if (boxTwo !== '' && (boxTwo === boxFive) && (boxTwo === boxEight)) {
-    return 'collumn 3 is winner'
-  } else if (boxZero !== '' && (boxZero === boxFour) && (boxZero === boxEight)) {
-    return 'diagonal right is winner'
-  } else if (boxTwo !== '' && (boxTwo === boxFour) && (boxFour === boxSix)) {
-    return 'diagonal left is winner'
-  } else {
-    return 'try again'
+  if (
+     (gameBoard[0] === playerSymbol && gameBoard[1] === playerSymbol && gameBoard[2] === playerSymbol) ||
+     (gameBoard[3] === playerSymbol && gameBoard[4] === playerSymbol && gameBoard[5] === playerSymbol) ||
+     (gameBoard[6] === playerSymbol && gameBoard[7] === playerSymbol && gameBoard[8] === playerSymbol) ||
+     (gameBoard[6] === playerSymbol && gameBoard[3] === playerSymbol && gameBoard[0] === playerSymbol) ||
+     (gameBoard[7] === playerSymbol && gameBoard[4] === playerSymbol && gameBoard[1] === playerSymbol) ||
+     (gameBoard[8] === playerSymbol && gameBoard[5] === playerSymbol && gameBoard[2] === playerSymbol) ||
+     (gameBoard[6] === playerSymbol && gameBoard[4] === playerSymbol && gameBoard[2] === playerSymbol) ||
+     (gameBoard[8] === playerSymbol && gameBoard[4] === playerSymbol && gameBoard[0] === playerSymbol)) {
+    console.log('we have a winner')
   }
+}
+module.exports = {
+  determineWinner
 }
