1e6777fd2016af10fb9674bafbf6c4924555b711
awoodrum87 <awoodrum87@gmail.com>
2017-04-07 14:27:52 -0400

    semi working game logic

--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -4,6 +4,7 @@ const getFormFields = require('../../../lib/get-form-fields')
 
 const api = require('./api')
 const ui = require('./ui')
+const index = require('../index.js')
 
 const onSignUp = function (event) {
   const data = getFormFields(this)
@@ -41,11 +42,38 @@ const onChangePassword = function (event) {
     .catch(ui.changePasswordFailure)
 }
 
+let playerSymbol = 'X'
+
+const onClickBoard = function () {
+  event.preventDefault()
+  console.log('works')
+  // ^ all 9 squares log 'works'
+  if ($(this).text() === ' ') {
+    $(this).text(playerSymbol)
+    if (playerSymbol === 'X') {
+      playerSymbol = 'O'
+    } else {
+      playerSymbol = 'X'
+    }
+  }
+  // this ^ if statement works - it stops a player from clicking
+  // on a game square multiple times and changing the the displayed playerSymbol
+}
+
 const addHandlers = () => {
   $('#sign-up').on('submit', onSignUp)
   $('#sign-in').on('submit', onSignIn)
   $('#sign-out').on('submit', onSignOut)
   $('#change-password').on('submit', onChangePassword)
+  $('#box-0').on('click', onClickBoard)
+  $('#box-1').on('click', onClickBoard)
+  $('#box-2').on('click', onClickBoard)
+  $('#box-3').on('click', onClickBoard)
+  $('#box-4').on('click', onClickBoard)
+  $('#box-5').on('click', onClickBoard)
+  $('#box-6').on('click', onClickBoard)
+  $('#box-7').on('click', onClickBoard)
+  $('#box-8').on('click', onClickBoard)
 }
 
 module.exports = {
--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -12,10 +12,60 @@ $(() => {
 
 // use require without a reference to ensure a file is bundled
 require('./example')
-
 const authEvents = require('./auth/events.js')
 
 // on document ready
 $(() => {
   authEvents.addHandlers()
 })
+
+const squaresPlayed = []
+// log the square that was clicked
+$('.gameBoard').on('click', '.boardPiece', function (event) {
+  // registers the current click
+  const currentClick = $(event.currentTarget)
+  // gets the index of the square that was just clicked
+  const indexOfSquare = $('.boardPiece').index(currentClick)
+  // gets the X or O
+  const stringInHTML = $(currentClick).html()
+  if (stringInHTML === 'X') {
+    squaresPlayed.push('X')
+  } else {
+    squaresPlayed.push('O')
+  }
+  console.log(squaresPlayed)
+  console.log('X or O in the DOM?:', stringInHTML)
+  console.log('indexOfSquare is: ', indexOfSquare)
+  console.log(winner())
+})
+const winner = function () {
+  const boxZero = $('#box-0').html()
+  const boxOne = $('#box-1').html()
+  const boxTwo = $('#box-2').html()
+  const boxThree = $('#box-3').html()
+  const boxFour = $('#box-4').html()
+  const boxFive = $('#box-5').html()
+  const boxSix = $('#box-6').html()
+  const boxSeven = $('#box-7').html()
+  const boxEight = $('#box-8').html()
+// logic to determine winners
+  if (boxZero !== '' && (boxZero === boxOne) && (boxZero === boxTwo)) {
+    return 'row 1 is winners'
+  } else if (boxThree !== '' && (boxThree === boxFour) && (boxThree === boxFive)) {
+    return 'row 2 is winner'
+  } else if (boxSix !== '' && (boxSix === boxSeven) && (boxSix === boxEight)) {
+    return 'row 3 is winner'
+  } else if (boxZero !== '' && (boxZero === boxThree) && (boxZero === boxSix)) {
+    return 'collumn 1 is winner'
+  } else if (boxOne !== '' && (boxOne === boxFour) && (boxOne === boxSeven)) {
+    return ('collumn 2 is winner')
+  } else if (boxTwo !== '' && (boxTwo === boxFive) && (boxTwo === boxEight)) {
+    return 'collumn 3 is winner'
+  } else if (boxZero !== '' && (boxZero === boxFour) && (boxZero === boxEight)) {
+    return 'diagonal right is winner'
+  } else if (boxTwo !== '' && (boxTwo === boxFour) && (boxFour === boxSix)) {
+    return 'diagonal left is winner'
+  } else {
+    return 'try again'
+  }
+}
