3b845ce9da8c70e4180ab454671c2a0f71c88ba9
awoodrum87 <awoodrum87@gmail.com>
2017-04-20 17:12:00 -0400

    completed HTML to hide and show divs

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -3,40 +3,42 @@
 const store = require('../store.js')
 
 const signUpSuccess = (data) => {
-  $('.sign-in-message').hide()
   $('.sign-up-message').text('Great! Now sign-in to start playing')
+  $('.sign-in-message').text('')
+  // $('.sign-up-message').fadeOut(4000)
 }
 
 const signUpFailure = (error) => {
   console.error(error)
-  $('.sign-in-message').text('Please create an account')
+  $('.sign-up-message').text('Please create an account')
+  // $('.sign-up-message').fadeOut(4000)
 }
 
 const signInSuccess = (data) => {
   store.user = data.user
   $('.sign-in-message').text('')
+  $('.sign-up-message').text('')
   $('.new-game').show()
   $('.new-game-banner').text('Click New Game to start')
 }
 
 const signInFailure = (error) => {
   console.error(error)
-  $('.sign-up-message').hide()
   $('.sign-in-message').text('You have not entered a registered name and password. Create an account or log-in')
+  // $('.sign-in-message').fadeOut(4000)
 }
 
 const signOutSuccess = (data) => {
   store.user = null
   $('.sign-out-message').text('You have signed out')
-  $('.game-board').hide()
+  $('.sign-out-message').fadeOut(4000)
   $('.new-game').hide()
-  $('.password-message-placeholder').hide()
-  $('.game-stats').hide()
-  $('.sign-in-message').show()
-  $('.sign-up-message').show()
-  $('.intro').hide()
+  $('.total-stats').hide()
   $('.change-password').hide()
   $('.sign-out').hide()
+  $('.game-board').hide()
+  $('.intro').hide()
+  $('.game-stats').hide()
   $('.sign-in').show()
   $('.sign-up').show()
 }
@@ -46,21 +48,19 @@ const signOutFailure = (error) => {
 }
 
 const changePasswordSuccess = (data) => {
-  // $('.password-message-placeholder').fadeOut('You have updated your password', 1000)
   $('.password-message-placeholder').text('You have updated your password')
-  $('.password-message-placeholder').fadeOut(2000)
+  $('.password-message-placeholder').fadeOut(4000)
 }
 
 const changePasswordFailure = (error) => {
   console.error(error)
   $('.password-message-placeholder').text('Make sure to enter your old password and a new one!')
+  $('.password-message-placeholder').fadeOut(4000)
 }
 
 const newGameSuccess = (data) => {
   store.game = data.game
-  $('.sign-in-message').text('')
   $('.new-game-banner').text('')
-  $('.sign-up-message').hide()
 }
 
 const newGameFailure = (error) => {
